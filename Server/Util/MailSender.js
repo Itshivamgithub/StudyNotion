55f85fcbf2e3f2c25b06ac07ef34cc5e5efd1c86
Itshivamgithub <shivamrajbhar883@gmail.com>
2026-06-01 21:08:49 +0530

    fix: revert to port 587 for SMTP as 465 is timing out on Render

--- Server/Util/MailSender.js
+++ Server/Util/MailSender.js
@@ -16,8 +16,8 @@ const mailSender = async (email, title, body) => {
 
     let transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
-      port: 465,
-      secure: true, // Use SSL
+      port: 587,
+      secure: false, // Use STARTTLS
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
